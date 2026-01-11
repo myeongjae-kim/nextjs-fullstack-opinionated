87ee256a8d711c92802b775c38a26c450dbf7bde
Myeongjae Kim (김명재) <dev@myeongjae.kim>
2026-01-12 02:09:00 +0900

    chore: update tests

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -7,9 +7,8 @@ dotenv.config();
 
 const TEST_HOST = 'http://localhost:3031';
 
-// 테스트를 위한 환경변수 설정은 여기에
+// 테스트용 환경변수 설정은 여기에
 vi.stubEnv('TEST_HOST', TEST_HOST);
-vi.stubEnv("TEST_BEARER_TOKEN", "default-token-value-for-docs")
 
 beforeEach(() => {
   request.setBaseUrl(TEST_HOST);
--- test/testEnv.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-const testEnvSchema = z.object({
-  TEST_HOST: z.url(),
-  TEST_BEARER_TOKEN: z.string(),
-})
-
-export const testEnv = testEnvSchema.parse(process.env);
